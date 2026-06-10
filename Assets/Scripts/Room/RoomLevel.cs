bb02a470e58b515b0438713ad0175189aebfe40e
Papia1323 <146428652+Alexander13217@users.noreply.github.com>
2026-06-10 22:52:16 +0300

    feat: add a few anomalies and level text

--- Assets/Scripts/Room/RoomLevel.cs
+++ Assets/Scripts/Room/RoomLevel.cs
@@ -1,18 +1,26 @@
+using TMPro;
 using UnityEngine;
 
 public class RoomLevel : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _levelText;
+
     private int _level = 1;
+
+    private void Awake()
+    {
+        _levelText.text = "1";
+    }
 
     public void Increase()
     {
         _level += 1;
-        Debug.Log($"Level: {_level}");
+        _levelText.text = System.Convert.ToString(_level);
     }
 
     public void Reset()
     {
         _level = 1;
-        Debug.Log($"Level: {_level}");
+        _levelText.text = System.Convert.ToString(_level);
     }
 }
